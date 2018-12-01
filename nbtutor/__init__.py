a6642a2586fe64e3d755ebb041d57dd7bec94ead
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2018-12-01 12:18:22 +0100

    Make solutions directory configurable

--- nbtutor/__init__.py
+++ nbtutor/__init__.py
@@ -10,14 +10,18 @@ try:
 except ImportError:
     from IPython.nbconvert.preprocessors.base import Preprocessor
 
+from traitlets import Unicode
+
 
 class ClearExercisePreprocessor(Preprocessor):
+
+    solutions_dir = Unicode("_solutions").tag(config=True)
 
     def preprocess_cell(self, cell, resources, index):
 
         if 'clear_cell' in cell.metadata and cell.metadata.clear_cell:
             fname = os.path.join(
-                '_solutions', resources['metadata']['name'] + str(cell['execution_count']) + '.py')
+                self.solutions_dir, resources['metadata']['name'] + str(cell['execution_count']) + '.py')
             with open(fname, 'w') as f:
                 f.write(cell['source'])
             cell['source'] = ["# %load {0}".format(fname)]
